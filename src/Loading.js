7e3f3d9ec507e485fe857a26be3a3f64b67dbaf9
surajpshenoy <surajpshenoy@gmail.com>
2021-07-20 17:48:41 +1000

    added loading image

--- src/Loading.js
+++ src/Loading.js
@@ -3,7 +3,8 @@ const Loading = () => {
 
     return (
         <div className="loading">
-            <h1>Please wait..page is loading</h1>
+            <h1>Please wait</h1>
+            <img src="https://media.giphy.com/media/N256GFy1u6M6Y/giphy.gif" alt="Loading"></img>
         </div>
     );
 }

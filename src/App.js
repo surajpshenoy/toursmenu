1ccea91f8dfca4cf77b1434b127906b9f6090a33
surajpshenoy <surajpshenoy@gmail.com>
2021-07-20 12:01:37 +1000

    Tours component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import React, {useState, useEffect} from "react"
 import './App.css';
 import Loading from './Loading';
+import Tours from "./Tours"
 
 function App() {
+
+  const [load, setLoading] = useState(false)
+  const [tours, setTours] = useState([]);
+  
+  if(load)
+  {
+    return ( 
+      <main>
+        <Loading />
+      </main>
+      
+    );
+  }
+
   return (
-    <Loading />
+    <main>
+      <Tours/>
+    </main>
+
   );
-}
+  }
+  
+
 
 export default App;
---
+++ src/Tours.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Tours = () => {
+    return (
+        <main>
+            <h2>Tour details:</h2>
+        </main>
+    );
+}
+
+export default Tours;

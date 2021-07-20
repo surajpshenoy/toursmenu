21ee2f3d3310e71327c5501ea85aa0bcb7d04a9f
surajpshenoy <surajpshenoy@gmail.com>
2021-07-20 12:45:03 +1000

    fetching data

--- src/App.js
+++ src/App.js
@@ -3,11 +3,31 @@ import './App.css';
 import Loading from './Loading';
 import Tours from "./Tours"
 
+
+//Adding the tour data from the api 
+const url = 'https://course-api.com/react-tours-project';
 function App() {
 
   const [load, setLoading] = useState(false)
   const [tours, setTours] = useState([]);
   
+  const fetchTourInfo = async () => {
+    setLoading(true)
+    try{
+      const response = await fetch(url);
+      const tours =  await response.json();
+      setLoading(false);
+      setTours(tours);
+    }
+    catch(error){
+      setLoading(false);
+    }
+   
+  }
+
+  useEffect(() => {
+    fetchTourInfo();
+  }, [])
   if(load)
   {
     return ( 

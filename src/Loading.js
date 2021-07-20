9d7c359ce251d93861ffe44175ec0b39ddfb2633
surajpshenoy <surajpshenoy@gmail.com>
2021-07-20 11:46:22 +1000

    Added loading page

---
+++ src/Loading.js
@@ -0,0 +1,12 @@
+import React from "react";
+const Loading = () => {
+
+    return (
+        <div className="loading">
+            <h1>Please wait..page is loading</h1>
+        </div>
+    );
+}
+
+
+export default Loading;
